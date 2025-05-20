f46870efffa2fc1f7a0acc56dbfae4747824d3b5
ankitsainibkbiet <ankitsainibkbiet2022@gmail.com>
2025-05-20 13:19:19 +0530

    Backend Project

---
+++ src/db/connection.js
@@ -0,0 +1,17 @@
+import dotenv from "dotenv"
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants.js";
+
+dotenv.config()
+
+const connectDB = async () => {
+    try{
+        await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`)
+        console.log("DB Connected")
+    }catch(error){
+        console.log("Connection Failure in DB : ", error)
+        process.exit(1)
+    }
+}
+
+export default connectDB
